05c45c99be3d1a555ca06a6450ea1810c9c72ffc
cchan <charles.chan@net-makers.com.hk>
2018-03-29 22:57:26 +0800

    fix bug

--- stages/assist.py
+++ stages/assist.py
@@ -106,7 +106,7 @@ def translate(s, lang='cn', var={}):
 	reload(sys)
 	sys.setdefaultencoding('utf-8')
 	result = translations[s][lang]
-	for k, v in var:
+	for k, v in var.iteritems():
 		tmp = re.split(k, result)
 		construct = ''
 		for x in range(0,len(tmp)-1):
